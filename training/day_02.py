4b1c9e49eca625f81404be8cc244c47eb6c7ed36
Malan <malanrautenbach777@gmail.com>
2024-08-21 22:33:03 +0200

    added some content for day 2 - Needs completion
    add the stubs for day two

--- training/day_02.py
+++ training/day_02.py
@@ -1,1 +1,23 @@
  # functions and loops
+
+def add(a, b):
+    return a + b
+
+answer = add(23, 78)
+print(answer)
+
+
+list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+# for item in list:
+#     print(item)
+
+# I want to add each item in the list together and print the result
+def add_items():
+    total = 0
+    for item in list:
+        total += item
+    print(total)
+
+add_items()
+
