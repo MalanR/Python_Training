3ad14ad2df7254043dc312e57111b3eadbd69e6b
Malan <malanrautenbach777@gmail.com>
2024-08-27 11:39:30 +0200

    measures challenge on day 4
    please store the new files you will create in the Measure_Challenge folder.

--- training/day_02.py
+++ training/day_02.py
@@ -7,3 +7,34 @@
 # you will need to call your methods through the process api.
 
 
+class ProcessAPI:
+    def __init__(self):
+        self.modules = {}
+
+    def add_module(self, name, module):
+        self.modules[name] = module
+
+    def call(self, module, method, params):
+        module = self.modules[module]
+        method = getattr(module, method)
+        return method(**params)
+
+
+class MathModule:
+
+    @staticmethod
+    def add(value1, value2):
+        return value1 + value2
+
+    @staticmethod
+    def subtract(value1, value2):
+        return value1 - value2
+
+
+api = ProcessAPI()
+api.add_module("math", MathModule)
+add_result = api.call("math", "add", {"value1": 5, "value2": 6})
+subtract_result = api.call("math", "subtract", {"value1": 6, "value2": 5})
+print(add_result)
+print(subtract_result)
+
--- training/day_04.py
+++ training/day_04.py
@@ -1,8 +1,73 @@
-# Mieliepap
+# objective:
+#     create a process api. (in its own file)
+#     register a module called measure and register a module called triggers.
+#     your measures module should handle the creation of measures. (this should be a module that also lives in its own file)
+#     your triggers module should handle the creation of triggers. (this should be a module that also lives in its own file)
+#     we then want to append a new key value pair into the triggers dictionary that looks like this {"measure_code": measure_code.}
+#     we then want to print out the result and verify that each trigger has this key value pair and that the code is equal to the measure code.
+#     expected json result:
+# data_structure = {
+#     "measurement": [
+#         {
+#             "code": uuid(oyfd9s8sdfjdf-sdfh7),
+#             "description": "Temperature",
+#             "triggers": [
+#                 {
+#                     "code": "T1",
+#                     "description": "Temperature is too high",
+#                     "measure_code": "oyfd9s8sdfjdf-sdfh7"
+#                 },
+#                 {
+#                     "code": "T2",
+#                     "description": "Temperature is too low",
+#                     "measure_code": "oyfd9s8sdfjdf-sdfh7"
+#                 }
+#             ]
+#         }
+# }
+#     keep separation of concern in mind.
+#    You will need to have a method in measuresModule that allows you to loop through the data structure
+#     You then want to grab the get the desired code, and assign it to the triggers.
+#     In your triggersModule you will also need a create measure that will loop through the triggers and assign the measure code to each trigger.
+#    in your main.py file you want to be able to use the process api to call the modules and measures needed to perform the task.
+#   Use the Measure_Challenge folder to store all you files.
 
-# 90/10 rule
-# Mieliepap is there to make you think about what you want to do
-# There are testcases and technical specifications
 
-# The goal is to write the document in a way that is as easy to read as possible.
-# This is not functional code, this is a list of instructions and requirement driven by the intent
+
+
+data_structure = {
+    "measurement": [
+        {
+            "code": uuid.uuid4,
+            "description": "Temperature",
+            "triggers": [
+                {
+                    "code": "T1",
+                    "description": "Temperature is too high",
+                },
+                {
+                    "code": "T2",
+                    "description": "Temperature is too low",
+                }
+            ]
+        },
+        {
+            "code": uuid.uuid4,
+            "description": "Humidity",
+            "triggers": [
+                {
+                    "code": "H1",
+                    "description": "Humidity is too high",
+                },
+                {
+                    "code": "H2",
+                    "description": "Humidity is too low",
+                },
+                {
+                    "code": "H3",
+                    "description": "Humidity is normal",
+                }
+            ]
+        }
+    ]
+}
